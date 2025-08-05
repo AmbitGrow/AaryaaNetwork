f4a3a9e90196a5de5903dfd214d7de99a2b54bd6
SABARISH-01 <sabarishvengadesh64@gmail.com>
2025-08-05 20:21:47 +0530

    Set trust proxy for correct detection of X-Forwarded-* headers

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1); // Or just `true` — enables correct detection of X-Forwarded-* headers
 
 app.use(express.json());
 app.use(
